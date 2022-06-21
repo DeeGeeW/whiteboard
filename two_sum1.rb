24353d232be91cdd7287f0d7ba2bcc98ec4a0a01
DeeGeeWest <DeeGeeWest@gmail.com>
2022-06-21 16:55:04 -0400

    two_sum

--- two_sum1.rb
+++ two_sum1.rb
@@ -1,4 +1,3 @@
-
 # Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 
 # Specifically use nested loops to solve this exercise even though there are other approaches as well.
@@ -9,8 +8,6 @@
 # Input: [1, 2, 3, 4, 5]
 # Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
 
-
-
 def array(nums)
   i = 0
   output = []
@@ -18,21 +15,18 @@ def array(nums)
   while i < nums.length
     f = 1
     if nums[i] != nums[f]
-      p nums[f]
       while f < nums.length
-      if nums[i] + nums[f] == 10
-        output << nums[i]
-        output << nums[f]
-        return output
-        break
+        if nums[i] + nums[f] == 10
+          output << nums[i]
+          output << nums[f]
+          return output
+          break
+        end
+        f += 1
       end
-      f +=1
     end
-      i +=1
-    end
+    i += 1
   end
 end
-
-
 
 p array([2, 5, 3, 1, 0, 7, 11])
